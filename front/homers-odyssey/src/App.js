daaa2fbe1ac9d59ab065007fe1cc3bea036ab174
Casey Heagerty <cheagerty@gmail.com>
2019-08-08 09:29:29 +0200

    quete 010

---
+++ front/homers-odyssey/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SignUp from "./SignUp/SignUp";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        <SignUp />
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ front/homers-odyssey/src/SignUp/SignUp.js
@@ -0,0 +1,32 @@
+import React, {Component} from "react";
+
+
+
+class SignUp extends Component {
+
+
+
+    state = {
+        email: 'test@test.com'
+    }
+
+
+    handleChange = (e) => {
+         this.setState({
+             email: e.target.value
+         })
+    }
+
+    render () {
+        return (
+            <div>
+                <form>
+                    <h1>{this.state.email}</h1>
+                    <input type="email" name="email" onChange={this.handleChange}/>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SignUp
